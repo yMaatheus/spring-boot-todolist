89d7c1c763308b60adb38fe86a7cf2efe6bc1e1c
Matheus <ymatheus.h@gmail.com>
2023-10-11 19:23:35 -0300

    feat: :sparkles: implements authorization middleware

--- src/main/java/me/ymaatheus/todolist/task/TaskController.java
+++ src/main/java/me/ymaatheus/todolist/task/TaskController.java
@@ -15,7 +15,6 @@ public class TaskController {
   
   @PostMapping("/")
   public TaskModel create(@RequestBody TaskModel taskModel) {
-
     var task = this.taskRepository.save(taskModel);
 
     return task;
